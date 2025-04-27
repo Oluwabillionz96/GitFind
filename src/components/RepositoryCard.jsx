e65b7333e9e44cd31c6c39a191586d85f604e030
oluwabillionz96 <goodluckreuben96@gmail.com>
2025-04-27 17:57:19 +0100

    update

--- src/components/RepositoryCard.jsx
+++ src/components/RepositoryCard.jsx
@@ -1,11 +1,20 @@
+import Linkify from "linkify-react";
 import "../styles/repositoryCard.css";
+import { Link } from "react-router-dom";
 
-const RepositoryCard = ({ name, language, desc }) => {
+const RepositoryCard = ({ name, language, desc, url }) => {
   return (
     <div className="repository-card">
       <h2>{name}</h2>
-      <p>{desc}</p>
-      <p>{language}</p>
+      <p className="desc">
+        <Linkify> {desc}</Linkify>
+      </p>
+      <footer>
+        <p className="language">{language}</p>
+        <Link to={url}>
+          <button>Visit Repository</button>
+        </Link>
+      </footer>
     </div>
   );
 };
--- src/pages/RepositoriesPage.jsx
+++ src/pages/RepositoriesPage.jsx
@@ -17,24 +17,53 @@ const RepositoriesPage = () => {
     });
   }, [data]);
 
-  const stillLoading = loading;
+  const languages = [];
+  const allLanguages = [];
+  function getAllLanguages() {
+    repositories?.forEach((repo) => {
+      allLanguages.push(repo?.language);
+    });
+  }
 
+  function clearRepeatedLanguages() {
+    allLanguages.forEach((language) => {
+      if (!languages.includes(language)) {
+        languages.push(language);
+      }
+    });
+  }
+
+  getAllLanguages();
+  clearRepeatedLanguages();
+
+  // repositories && console.log(allLanguages);
   return (
     <>
-      {stillLoading ? (
+      <div>
+        <button>All</button>
+        {languages?.map((language) => (
+          <li key={language}>
+            <button>{language}</button>
+          </li>
+        ))}
+      </div>
+      {loading ? (
         "Still Loading..."
       ) : error.isError ? (
         <p>{error.message}</p>
       ) : (
-        repositories?.map((repos) => (
-          <li key={repos.id}>
-            <RepositoryCard
-              name={repos?.name}
-              language={repos?.language}
-              desc={repos?.description}
-            />
-          </li>
-        ))
+        <ul className="repositories-container">
+          {repositories?.map((repos) => (
+            <li key={repos.id}>
+              <RepositoryCard
+                name={repos?.name}
+                language={repos?.language}
+                desc={repos?.description}
+                url={repos?.url}
+              />
+            </li>
+          ))}
+        </ul>
       )}
     </>
   );
